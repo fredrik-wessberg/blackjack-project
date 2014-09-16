7d468403ce3b1ef7f85029f39962bf2996e2e02f
Mitra <Mitra@Jimmys-MacBook-Pro-2.local>
2014-09-16 22:14:18 +0200

    Defined deck

--- js/deck.js
+++ js/deck.js
@@ -1,4 +1,6 @@
-//deckOfCards();
+//deckOfCards()
+var value;
+var family;
 
 
 
@@ -6,9 +8,10 @@
 var value = Array('A', '2', '3', '4', '5', '6', '7', '8', '9',
 	'10', 'J', 'Q', 'K');
 
-//this is the cards "famlily" Clubs, Diamonds, Spade.
+//this is the cards "famlily" Clubs, Diamonds, Spade, Hearts.
 var family = Array('C', 'D', 'H', 'S');
 
+//this becomes one deck of card 52 pcs.
 var deck = value.length * family.length;
 
-alert(deck);
+var x = 6; // the var x is the number of decks the game should have
