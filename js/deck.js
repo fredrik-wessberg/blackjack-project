9bdfa620170e9ddfa2a6dbe6aa27168241e4b345
Mitra <Mitra@Jimmys-MacBook-Pro-2.local>
2014-09-16 17:03:49 +0200

    make array of cardvalue and cardfamily, connected html with js

---
+++ js/deck.js
@@ -0,0 +1,10 @@
+deckOfCards();
+
+
+
+// this sets the value of the cards.
+var value = Array('A', '2', '3', '4', '5', '6', '7', '8', '9',
+	'10', 'J', 'Q', 'K');
+
+//this is the cards "famlily" Clubs, Diamonds, Spade.
+var family = Array('C', 'D', 'H', 'S');
